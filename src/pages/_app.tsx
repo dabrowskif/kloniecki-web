d976bdac0abf55c460743dcfd8508b4490c91609
dabrowskif <filip.dabrowski@protonmail.com>
2023-04-01 01:10:00 +0200

    test: deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,21 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { useEffect } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
