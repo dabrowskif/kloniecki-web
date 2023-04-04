91003195765cf0741030096064acb70e132bebd7
dabrowskif <filip.dabrowski@protonmail.com>
2023-04-04 18:18:15 +0200

    feat: google access token now refreshes with session

--- src/pages/api/googletest.ts
+++
@@ -1,9 +0,0 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  await Promise.resolve();
-  res.status(200).json({ name: "John Doe" });
-}
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,10 +3,11 @@ import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
+  type TokenSet,
+  type Session,
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import jsonwebtoken from "jsonwebtoken";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
@@ -18,6 +19,7 @@ import { prisma } from "~/server/db";
  */
 declare module "next-auth" {
   interface Session extends DefaultSession {
+    error?: "RefreshAccessTokenError";
     user: {
       id: string;
       email: string;
@@ -25,23 +27,27 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
+  // interface JWT {
+  //   access_token: string;
+  //   expires_at: number;
+  //   refresh_token: string;
+  //   error?: "RefreshAccessTokenError";
+  // }
+
+  interface User {
+    google_access_token?: string;
+  }
+
+  // interface TokenSet {
+  //   expires_in: number;
   // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     async session({ session, user }) {
-      // console.log(user);
-      // console.log(session);
       if (session.user) {
+        await refreshGoogleAccessToken(session);
         session.user.id = user.id;
         const data = await prisma.account.findFirst({
           where: { user: { email: user.email } },
@@ -97,4 +103,54 @@ export const getServerAuthSession = (ctx: {
   res: GetServerSidePropsContext["res"];
 }) => {
   return getServerSession(ctx.req, ctx.res, authOptions);
+};
+
+const refreshGoogleAccessToken = async (session: Session) => {
+  const userId = session.user.id;
+
+  const [google] = await prisma.account.findMany({
+    where: { userId, provider: "google" },
+  });
+
+  if (
+    google?.expires_at &&
+    google?.refresh_token &&
+    google.expires_at * 1000 < Date.now()
+  ) {
+    try {
+      const response = await fetch("https://oauth2.googleapis.com/token", {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+          client_id: env.GOOGLE_CLIENT_ID,
+          client_secret: env.GOOGLE_CLIENT_SECRET,
+          grant_type: "refresh_token",
+          refresh_token: google.refresh_token,
+        }),
+        method: "POST",
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const tokens: TokenSet & { expires_in: number } = await response.json();
+
+      if (!response.ok) throw tokens;
+      if (tokens?.expires_in) {
+        await prisma.account.update({
+          data: {
+            access_token: tokens.access_token,
+            expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
+            refresh_token: tokens.refresh_token ?? google.refresh_token,
+          },
+          where: {
+            provider_providerAccountId: {
+              provider: "google",
+              providerAccountId: google.providerAccountId,
+            },
+          },
+        });
+      }
+    } catch (error) {
+      console.error("❌ Error refreshing access token", error);
+      session.error = "RefreshAccessTokenError";
+    }
+  }
 };
